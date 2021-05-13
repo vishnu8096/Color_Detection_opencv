793fc906ac73a13fad37b3642cc2aaf0773bada3
Vishnu Vardhan Reddy. Gandavarapu <58591109+vishnu8096@users.noreply.github.com>
2021-05-13 22:04:14 +0530

    updated

---
+++ main.py
@@ -0,0 +1,67 @@
+import cv2
+import numpy as np
+import pandas as pd
+
+img_path = "images.jpg"
+img = cv2.imread(img_path)
+img = cv2.resize(img, (700, 500))
+
+clicked = False
+r = g = b = xpos = ypos = 0
+
+
+index = ["color", "color_name", "hex", "R", "G", "B"]
+csv = pd.read_csv('colors.csv', names=index, header=None)
+
+
+
+def getColorName(R, G, B):
+    minimum = 10000
+    for i in range(len(csv)):
+        d = abs(R - int(csv.loc[i, "R"])) + abs(G - int(csv.loc[i, "G"])) + abs(B - int(csv.loc[i, "B"]))
+        if (d <= minimum):
+            minimum = d
+            cname = csv.loc[i, "color_name"]
+    return cname
+
+
+
+def draw_function(event, x, y, flags, param):
+    if event == cv2.EVENT_LBUTTONDBLCLK:
+        global b, g, r, xpos, ypos, clicked
+        clicked = True
+        xpos = x
+        ypos = y
+        b, g, r = img[y, x]
+        b = int(b)
+        g = int(g)
+        r = int(r)
+
+
+cv2.namedWindow('color detection')
+cv2.setMouseCallback('color detection', draw_function)
+
+while True:
+
+    cv2.imshow("color detection", img)
+    if (clicked):
+
+        # cv2.rectangle(image, startpoint, endpoint, color, thickness)-1 fills entire rectangle
+        cv2.rectangle(img, (20, 20), (750, 60), (b, g, r), -1)
+
+        # Creating text string to display( Color name and RGB values )
+        text = getColorName(r, g, b) + ' R=' + str(r) + ' G=' + str(g) + ' B=' + str(b)
+
+        # cv2.putText(img,text,start,font(0-7),fontScale,color,thickness,lineType )
+        cv2.putText(img, text, (50, 50), 2, 0.8, (255, 255, 255), 2, cv2.LINE_AA)
+
+        # For very light colours we will display text in black colour
+        if (r + g + b >= 600):
+            cv2.putText(img, text, (50, 50), 2, 0.8, (0, 0, 0), 2, cv2.LINE_AA)
+
+        clicked = False
+
+    if cv2.waitKey(1)==13:
+        break
+
+cv2.destroyAllWindows()
